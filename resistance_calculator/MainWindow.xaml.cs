63981217c3cc369ad7c55dab84aaebb0296fa668
d2h10s <ecanus9@gmail.com>
2021-06-12 16:22:48 +0900

    version 1.0.2

--- resistance_calculator/MainWindow.xaml.cs
+++ resistance_calculator/MainWindow.xaml.cs
@@ -49,15 +49,15 @@ namespace resistance_calculator
             gd_type.ColumnDefinitions.Add(col2);
             gd_type.ColumnDefinitions.Add(col3);
 
-            tb_output.Text = "\nAuthor: 서울과학기술대학교 최호승\n" +
+            tb_output.Text = "Author: d2h10s\n" +
                 "Github: https://github.com/d2h10s \n" +
-                "Version: 1.0.1\n" +
+                "Version: 1.0.2\n" +
                 "--------------------사용법--------------------\n" +
                 "1. 저항 수를 정합니다.\n" +
-                "2. 저항값을 입력합니다.\n" +
-                "3-1. 직렬 합성 버튼을 누르면 직렬 연결시의 합성 저항 값을 보여줍니다.\n" +
-                "3-2. 병렬 합성 버튼을 누르면 병렬 연결시의 합성 저항 값을 보여줍니다.\n" +
-                "3-3. 조합 찾기 버튼을 누르면 현재 입력한 저항들로 목표값을 만들 수 있는 모든 조합을 보여줍니다.\n" +
+                "2. 저항값을 입력합니다.\n\n" +
+                "3-1. [직렬 합성] 버튼을 누르면 직렬 연결시의 합성 저항 값을 보여줍니다.\n\n" +
+                "3-2. [병렬 합성] 버튼을 누르면 병렬 연결시의 합성 저항 값을 보여줍니다.\n\n" +
+                "3-3. [조합 찾기] 버튼을 누르면 현재 입력한 저항들로 목표값을 만들 수 있는 모든 조합을 보여줍니다.\n\n" +
                 "------------------------------------------------\n\n";
         }
 
@@ -126,7 +126,8 @@ namespace resistance_calculator
                     return;
                 }
             }
-            tb_output.Text += $"직렬 합성 저항의 계산값은 {s} Ω 입니다.\n\n";
+            tb_output.Text += $"직렬 합성 저항의 계산값은 {s} Ω 입니다." +
+                $"\n------------------------------------------------\n\n";
             tb_output.ScrollToEnd();
         }
         private void parallel_combine_Click(object sender, RoutedEventArgs e)
@@ -146,7 +147,8 @@ namespace resistance_calculator
                     return;
                 }
             }
-            tb_output.Text += $"병렬 합성 저항의 계산값은 {1/s:.000} Ω 입니다.\n\n";
+            tb_output.Text += $"병렬 합성 저항의 계산값은 {1/s:.000} Ω 입니다." +
+                $"\n------------------------------------------------\n\n";
             tb_output.ScrollToEnd();
         }
 
@@ -187,19 +189,20 @@ namespace resistance_calculator
             if (int.TryParse(target.Text, out int t)){
                 target_value = t;
                 dfs(0, 0, 0, new List<int>());
-                if (result.Count == 0) tb_output.Text += "가능한 조합이 없습니다.\n\n";
+                if (result.Count == 0) tb_output.Text += "가능한 조합이 없습니다.";
                 else
                 {
                     tb_output.Text += $"###  {target_value}Ω 을 만들 수 있는 저항 조합  ###\n";
                     int i = 1;
                     foreach (var a in result)
                     {
-                        tb_output.Text += $"조합{i} -> ";
+                        int j = 1;
+                        tb_output.Text += $"조합{i++} -> ";
                         foreach (var b in a)
                         {
-                            tb_output.Text += $"{b}Ω" + (i++ < a.Count? ", ":"");
+                            tb_output.Text += $"{b}Ω" + (j++ < a.Count ? ", " : "");
                         }
-                        tb_output.Text += "\n\n";
+                        tb_output.Text += "\n";
                         tb_output.ScrollToEnd();
                     }
                 }
@@ -209,6 +212,7 @@ namespace resistance_calculator
                 tb_output.Text += "저항값은 숫자만 가능합니다.\n\n";
                 SystemSounds.Beep.Play();
             }
+            tb_output.Text += "\n------------------------------------------------\n\n";
             tb_output.ScrollToEnd();
         }
     }
